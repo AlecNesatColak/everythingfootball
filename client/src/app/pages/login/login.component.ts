4420ea86c1dd34e76da44882cc084db662810ca2
Alec-Nesat Colak <acolak9@ufl.edu>
2024-05-14 10:32:33 -0400

    added bootstrap and login form

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../../auth.service';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.log(this.loginForm.value);
+    console.log(this.loginForm.value.username);
+    console.log(this.loginForm.value.email);
+    console.log(this.loginForm.value.password);
+
+  }
 }
